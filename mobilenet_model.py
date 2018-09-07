0dcb2958facab8035953c0e1587562709aee6501
Jeffen Chen <jeffen.chen@OTTW22.local>
2018-09-07 10:23:22 -0400

    adjust mobilenet model

--- mobilenet_model.py
+++ mobilenet_model.py
@@ -1,81 +1,147 @@
-"""
-Train the MobileNet V2 model
-"""
-import os
-import sys
-import argparse
-import pandas as pd
-import numpy as np
+"""MobileNet v2 models for Keras.
 
-from mobilenet_model import MobileNetv2
+# Reference
+- [Inverted Residuals and Linear Bottlenecks Mobile Networks for
+   Classification, Detection and Segmentation]
+   (https://arxiv.org/abs/1801.04381)
+"""
 
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-from keras.layers import Conv2D, Reshape, Activation
+
 from keras.models import Model
+from keras.layers import Input, Conv2D, GlobalAveragePooling2D, Dropout
+from keras.layers import Activation, BatchNormalization, add, Reshape
+from keras.applications.mobilenet import relu6, DepthwiseConv2D
+from keras.utils.vis_utils import plot_model
 
-from sklearn.metrics import confusion_matrix
+from keras import backend as K
 
-def fine_tune(model):
-    """Re-build model with current num_classes.
+
+def _conv_block(inputs, filters, kernel, strides):
+    """Convolution Block
+    This function defines a 2D convolution operation with BN and relu6.
 
     # Arguments
-        model, Model, The model structure.
+        inputs: Tensor, input tensor of conv layer.
+        filters: Integer, the dimensionality of the output space.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+        strides: An integer or tuple/list of 2 integers,
+            specifying the strides of the convolution along the width and height.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+
+    # Returns
+        Output tensor.
     """
-    model.load_weights('saved_models/weights.best.mbnetv2.hdf5')
 
-    x = model.get_layer('Dropout').output
-    x = Conv2D(100, (1, 1), padding='same')(x)
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+
+    x = Conv2D(filters, kernel, padding='same', strides=strides)(inputs)
+    x = BatchNormalization(axis=channel_axis)(x)
+    return Activation(relu6)(x)
+
+
+def _bottleneck(inputs, filters, kernel, t, s, r=False):
+    """Bottleneck
+    This function defines a basic bottleneck structure.
+
+    # Arguments
+        inputs: Tensor, input tensor of conv layer.
+        filters: Integer, the dimensionality of the output space.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+        t: Integer, expansion factor.
+            t is always applied to the input size.
+        s: An integer or tuple/list of 2 integers,specifying the strides
+            of the convolution along the width and height.Can be a single
+            integer to specify the same value for all spatial dimensions.
+        r: Boolean, Whether to use the residuals.
+
+    # Returns
+        Output tensor.
+    """
+
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+    tchannel = K.int_shape(inputs)[channel_axis] * t
+
+    x = _conv_block(inputs, tchannel, (1, 1), (1, 1))
+
+    x = DepthwiseConv2D(kernel, strides=(s, s), depth_multiplier=1, padding='same')(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+    x = Activation(relu6)(x)
+
+    x = Conv2D(filters, (1, 1), strides=(1, 1), padding='same')(x)
+    x = BatchNormalization(axis=channel_axis)(x)
+
+    if r:
+        x = add([x, inputs])
+    return x
+
+
+def _inverted_residual_block(inputs, filters, kernel, t, strides, n):
+    """Inverted Residual Block
+    This function defines a sequence of 1 or more identical layers.
+
+    # Arguments
+        inputs: Tensor, input tensor of conv layer.
+        filters: Integer, the dimensionality of the output space.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+        t: Integer, expansion factor.
+            t is always applied to the input size.
+        s: An integer or tuple/list of 2 integers,specifying the strides
+            of the convolution along the width and height.Can be a single
+            integer to specify the same value for all spatial dimensions.
+        n: Integer, layer repeat times.
+    # Returns
+        Output tensor.
+    """
+
+    x = _bottleneck(inputs, filters, kernel, t, strides)
+
+    for i in range(1, n):
+        x = _bottleneck(x, filters, kernel, t, 1, True)
+
+    return x
+
+
+def MobileNetv2(input_shape, k):
+    """MobileNetv2
+    This function defines a MobileNetv2 architectures.
+
+    # Arguments
+        input_shape: An integer or tuple/list of 3 integers, shape
+            of input tensor.
+        k: Integer, number of classes.
+    # Returns
+        MobileNetv2 model.
+    """
+
+    inputs = Input(shape=input_shape)
+    x = _conv_block(inputs, 32, (3, 3), strides=(2, 2))
+
+    x = _inverted_residual_block(x, 16, (3, 3), t=1, strides=1, n=1)
+    x = _inverted_residual_block(x, 24, (3, 3), t=6, strides=2, n=2)
+    x = _inverted_residual_block(x, 32, (3, 3), t=6, strides=2, n=3)
+    x = _inverted_residual_block(x, 64, (3, 3), t=6, strides=2, n=4)
+    x = _inverted_residual_block(x, 96, (3, 3), t=6, strides=1, n=3)
+    x = _inverted_residual_block(x, 160, (3, 3), t=6, strides=2, n=3)
+    x = _inverted_residual_block(x, 320, (3, 3), t=6, strides=1, n=1)
+
+    x = _conv_block(x, 1280, (1, 1), strides=(1, 1))
+    x = GlobalAveragePooling2D()(x)
+    x = Reshape((1, 1, 1280))(x)
+    x = Dropout(0.3, name='Dropout')(x)
+    x = Conv2D(k, (1, 1), padding='same')(x)
+
     x = Activation('softmax', name='softmax')(x)
-    output = Reshape((100,))(x)
+    output = Reshape((k,))(x)
 
-    model = Model(inputs=model.input, outputs=output)
+    model = Model(inputs, output)
+    plot_model(model, to_file='images/MobileNetv2.png', show_shapes=True)
 
     return model
 
 
-def train(batch, epochs, num_classes, size, weights, train_tensors, train_targets, valid_tensors, valid_targets):
-    """Train the model.
-
-    # Arguments
-        batch: Integer, The number of train samples per batch.
-        epochs: Integer, The number of train iterations.
-        num_classes, Integer, The number of classes of dataset.
-        size: Integer, image size.
-        weights, Boolean, The pre_trained model weights.
-    """
-    if not os.path.exists('saved_models'):
-        os.makedirs('saved_models')
-
-    if weights:
-        model = MobileNetv2((size, size, 3), num_classes)
-        model = fine_tune(model)
-    else:
-        model = MobileNetv2((size, size, 3), num_classes)
-
-    opt = Adam()
-    earlystop = EarlyStopping(monitor='val_acc', patience=15, verbose=0, mode='auto')
-    checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.mbnetv2.hdf5', 
-                                verbose=1, save_best_only=True)
-
-    model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
-
-    hist = model.fit(train_tensors, train_targets,
-            validation_data=(valid_tensors, valid_targets),
-            epochs=epochs, verbose=2, batch_size=batch, callbacks=[earlystop, checkpointer])
-
-    df = pd.DataFrame.from_dict(hist.history)
-    df.to_csv('saved_models/hist_mbnet.csv', encoding='utf-8', index=False)
-    
-    
-def score(size, num_classes, test_tensors, test_targets):
-    model = MobileNetv2((size, size, 3), num_classes)
-    model.load_weights('saved_models/weights.best.mbnetv2.hdf5')
-    # get index of predicted dog breed for each image in test set
-    predictions = [np.argmax(model.predict(np.expand_dims(tensor, axis=0))) for tensor in test_tensors]
-
-    confusion_matrix(np.argmax(test_targets, axis=1), np.array(predictions), labels=range(99))
-    # report test accuracy
-    test_accuracy = 100*np.sum(np.array(predictions)==np.argmax(test_targets, axis=1))/len(predictions)
-    print('Test accuracy: %.4f%%' % test_accuracy)
+if __name__ == '__main__':
+    MobileNetv2((224, 224, 3), 100)
